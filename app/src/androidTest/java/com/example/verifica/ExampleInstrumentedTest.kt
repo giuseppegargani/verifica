234b5615c3bb317245f0daa40bf12ebb45149ac1
giuseppegargani <sentierodisole1@yahoo.it>
2022-04-05 19:12:42 +0200

    aggiunta

--- app/src/androidTest/java/com/example/verifica/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/verifica/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.example.verifica
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +8,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Exception
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +19,35 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
+    /*@Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.verifica", appContext.packageName)
+    }*/
+
+    @Test
+    fun A_useAppContext() {
+        //nameTest= "useAppContext"
+        try {
+            // Context of the app under test.
+            val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+            assertEquals("com.example.verifica", appContext.packageName)
+
+            //una volta verificato si dovrebbe poter scrivere
+            val context= InstrumentationRegistry.getInstrumentation().targetContext
+            val path: File = context.getExternalFilesDir(null)!!
+            Log.d("giuseppe", "nome directory $path")
+            val file = File(path, "giuseppe.txt")
+            val stream = FileOutputStream(file)
+            stream.use { stream ->
+                stream.write("A_useAppContextTest successo".toByteArray())
+            }
+            //scriviConsole("A_useAppContextTest")
+            //Log.d("giuseppe", "Test $nameTest riuscito!!!")
+        }
+        catch (exception: Exception){
+            //Log.d("giuseppe", "Test $nameTest non riuscito!!! e ha generato una eccezione")
+        }
     }
 }
